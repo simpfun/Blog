42d2bfacfe4f7e2311f3e72ef4caf4946121e0b7
MliKiowa <nineto0@163.com>
2022-11-13 08:27:30 +0800

    Update Deploy.sh

--- script/Deploy.sh
+++ script/Deploy.sh
@@ -1,13 +1,1 @@
-GITHUB_URL=https://oauth:${GITHUB_TOKEN}@github.com/simpfun/simpfun.github.io.git
-git clone ${GITHUB_URL} temp
-rm -rf temp/*
-mv public/* temp
-cd temp 
-git config --global init.defaultBranch main
-git remote add origin ${GITHUB_URL}
-git branch -M main
-git config --global user.name "simpfun"
-git config --global user.email "2382752120@qq.com"
-git add *
-git commit -m "Automatic generation"
-git push --set-upstream origin main
+
